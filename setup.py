c26d3f0a4d318f2e8c638670585a503e75a303d8
Jack Roberts <jroberts@turing.ac.uk>
2019-05-24 18:16:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A lightweight package for generating visually distinct colours.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alan-turing-institute/distinctipy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
