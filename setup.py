808525b4776c127cde364fb6844f1553c8184e00
Jack Roberts <jroberts@turing.ac.uk>
2019-05-24 16:24:42 +0100

    fix directory structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="distinctipy",
+    version="1.0",
+    author="Jack Roberts",
+    author_email="jroberts@turing.ac.uk",
+    description="A lightweight package for generating visually distinct colours.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
