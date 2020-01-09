6103d6bce1fd9feb827f67f42d79460576a939ec
Jack Roberts <jroberts@turing.ac.uk>
2020-01-09 13:07:29 +0000

    Update test_distinctipy.py

--- tests/test_distinctipy.py
+++ tests/test_distinctipy.py
@@ -2,5 +2,7 @@ from distinctipy import distinctipy
 
 
 def test_get_random_color():
+    """Assert all r,g,b components are between 0 and 1"""
     color = distinctipy.get_random_color()
     assert all([x >= 0 and x <= 1 for x in color])
+
