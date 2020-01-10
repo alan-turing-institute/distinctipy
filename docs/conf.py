9a891e5e74f6d20b7dcc9b1b9c9d104e9fdc509e
Jack Roberts <jroberts@turing.ac.uk>
2020-01-10 13:16:36 +0000

    update config for readthedocs builds

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# to fix readthedocs build errors
+# see https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
+master_doc = 'index'
