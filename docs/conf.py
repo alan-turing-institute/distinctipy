76be0c6103fbbdac8f637a74a954a07c86a17ec0
jack89roberts <jroberts@turing.ac.uk>
2022-07-06 21:10:28 +0100

    bump version string in docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = "2020, Jack Roberts"
 author = "Jack Roberts"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.1"
+release = "v1.2.2"
 
 
 # -- General configuration ---------------------------------------------------
